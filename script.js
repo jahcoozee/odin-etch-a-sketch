c44978584d52fb8de274c691914fc82d9ccb6a30
dj-runas <93562682+dj-runas@users.noreply.github.com>
2023-02-22 11:25:36 -0800

    Add event listener to paint on canvas

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const container = document.querySelector(".container");
+
+// Build canvas to populate with pixel divs
 const canvas = new DocumentFragment();
 
-for (i = 0; i < 256; i++) {
+// Populate canvas with pixels based on grid size
+for (i = 0; i < 4096; i++) {
   const pixel = document.createElement("div");
+  pixel.classList.add("pixel");
   canvas.append(pixel);
 }
 
+// Change color of pixels in grid
+let pixels = canvas.querySelectorAll(".pixel");
+pixels.forEach(pixel => pixel.addEventListener("mouseenter", () => {
+  pixel.classList.add("paint");
+}));
+
+// Call last to draw
 container.append(canvas);
