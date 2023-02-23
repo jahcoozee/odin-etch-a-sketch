6bb45fec51f7a3d56e8519f74f1533a141cd4715
dj-runas <93562682+dj-runas@users.noreply.github.com>
2023-02-22 16:11:06 -0800

    Add ability to change grid size

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
+// Build canvas to populate with pixel divs
 const container = document.querySelector(".container");
 
-// Build canvas to populate with pixel divs
-const canvas = new DocumentFragment();
-
-// Populate canvas with pixels based on grid size
-for (i = 0; i < 4096; i++) {
+// Populate canvas with a default grid size of 16x16
+for (i = 0; i < 256; i++) {
   const pixel = document.createElement("div");
   pixel.classList.add("pixel");
-  canvas.append(pixel);
+  container.appendChild(pixel);
 }
 
 // Change color of pixels in grid
-let pixels = canvas.querySelectorAll(".pixel");
+let pixels = container.querySelectorAll(".pixel");
 pixels.forEach(pixel => pixel.addEventListener("mouseenter", () => {
   pixel.classList.add("paint");
 }));
 
-// Call last to draw
-container.append(canvas);
+// Allow user to input grid size and remake the canvas
+const gridSizeButton = document.querySelector(".grid-size");
+gridSizeButton.addEventListener("click", setGridSize);
+
+function setGridSize() {
+  buildNewCanvas();
+
+  // Request new grid size from user
+  let size = prompt("Select a new grid size (between 1-100):");
+
+  // Populate new canvas based on user input
+  for (i = 0; i < size * size; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    container.appendChild(pixel);
+  }
+
+  // Adjust number of columns based on user input
+  const gridColumns = document.querySelector(".container");
+  gridColumns.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+  // Change color of pixels in grid
+  let pixels = container.querySelectorAll(".pixel");
+  pixels.forEach(pixel => pixel.addEventListener("mouseenter", () => {
+    pixel.classList.add("paint");
+  }));
+}
+
+// Clear default grid size of 16x16
+function buildNewCanvas() {
+  const pixel = document.createElement("div");
+  container.replaceChildren();
+}
